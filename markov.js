25eb7ac1a2141f1925fe9c17cd34147e7afc7f1e
Nick Aceves <nickaceves@gmail.com>
2013-06-26 23:17:35 -0500

    Woot, generating some stufs.

--- markov.js
+++ markov.js
@@ -1,21 +1,66 @@
 (function() {
   window.MarkovGenerator = function() {
+    function choose(p) {
+      var c = Math.floor(Math.random() * p.__total);
+      
+      var accum = 0;
+      for(var k in p) {
+        if (k === '__total') continue;
+        
+        accum += p[k];
+        if (accum > c)
+          return k;
+      }
+    };
+  
     this.generate = function() {
+      var result = '';
+      
+      var current = '$start'
+      
+      while(current !== '$end') {
+        var possible = incidents[current];
+        current = choose(possible);
+        
+        if (current !== '$end') {
+          result += current + ' ';
+        }
+      }
+      
+      return result;
     };
     
-    var regex = /(\w+)((\W+)|$)/gm.compile();
+    var regex = /(\w+)/gm;
     
     function tokenize(s) {
       var tokens = [];
       var match;
       while(match = regex.exec(s)) {
-        tokens.push(match);
+        tokens.push(match[0]);
       }
       return tokens;
     };
     
+    var incidents = {};
+    this.incidents = incidents;
+    
     this.train = function(sample) {
-      return tokenize(sample);
+      var tokens = tokenize(sample);
+      tokens.push('$end');
+      
+      var current = '$start', last;
+      
+      for(var i = 0; i < tokens.length; i++) {
+        last = current;
+        current = tokens[i];
+        
+        if (typeof incidents[last] === 'undefined') {
+          incidents[last] = {};
+        }
+        
+        incidents[last][current] = incidents[last][current] + 1 || 1;
+        incidents[last].__total = incidents[last].__total + 1 || 1;
+      }
     };
   };
 })();
